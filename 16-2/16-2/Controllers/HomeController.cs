2d71920dba92ab5e568d12309d2f0f070999d771
Toqa Khashashneh <toqamusa@gmail.com>
2025-02-17 00:51:07 +0300

    MVC

---
+++ 16-2/16-2/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using _16_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _16_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+
+
+        public IActionResult LegalMatters()
+        {
+            return View();
+        }
+
+
+        public IActionResult Properties()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 16-2/16-2/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _16_2.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
