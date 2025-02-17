ace404fe39d62b7296fe64bcf3be47a829fed755
Toqa Khashashneh <toqamusa@gmail.com>
2025-02-17 23:15:20 +0300

    task2

---
+++ 17-2/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _17_2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
