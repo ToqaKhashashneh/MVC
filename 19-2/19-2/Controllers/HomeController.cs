6fb0f0fcf05e5b04a09e8363d274ec6ba53be263
Toqa Khashashneh <toqamusa@gmail.com>
2025-02-20 00:39:03 +0300

    task(3)

---
+++ 19-2/19-2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _19_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _19_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.msg = HttpContext.Session.GetString("UserName");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 19-2/19-2/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _19_2.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HandleRegister(string name, string email , string password , string confpassword)
+        {
+            HttpContext.Session.SetString("UserName", name);
+            HttpContext.Session.SetString("UserEmail", email);
+            HttpContext.Session.SetString("UserPassword", password);
+            HttpContext.Session.SetString("UserConfPassword", confpassword);
+
+            if (password != confpassword)
+            {
+                TempData["RegisterMsg"] = "Password doesn't match! Try Again.";
+                return RedirectToAction("Register");
+
+            }
+            else
+            {
+                return RedirectToAction("Login");
+
+            }
+        }
+        [HttpPost]
+        public IActionResult HandleLogin(string email , string password)
+        {
+            string userEmail = HttpContext.Session.GetString("UserEmail");
+            string userPassword = HttpContext.Session.GetString("UserPassword");
+
+            if (email == userEmail && password == userPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["LoginMsg"] = "Invalid Email or Password! Try Again.";
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Profile()
+        {
+            ViewBag.UserName = HttpContext.Session.GetString("UserName");
+
+            ViewBag.UserEmail = HttpContext.Session.GetString("UserEmail");
+            ViewBag.UserPassword = HttpContext.Session.GetString("UserPassword");
+            return View();
+        }
+    }
+}
