4c4fcdce979af56b504659459356a1286fb0323d
Toqa Khashashneh <toqamusa@gmail.com>
2025-02-23 22:42:30 +0300

    task

--- 23-2test/23-2test/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _23_2test.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
